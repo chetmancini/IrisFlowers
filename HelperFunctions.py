92b7da0fa3d8c50261f748cd36c68b6128a9e1f7
Chet Mancini <chetmancini@gmail.com>
2011-11-18 17:56:50 -0500

    added helper functions

---
+++ HelperFunctions.py
@@ -0,0 +1,57 @@
+'''
+dot product
+'''
+def dot(vect1, vect2):
+	toReturn = 0
+	if len(vect1) != len(vect2):
+		print 'unmatched vectors'
+	for i in range(len(vect1)):
+		toReturn += vect1[i] * vect2[i]
+	return toReturn
+
+'''
+multiply a vector by a scalar
+'''
+def multVect(scalar, vect):
+	return [x * scalar for x in vect]
+
+
+'''
+add two vectors together --> vector
+'''
+def addVect(vect1, vect2):
+	toReturn = []
+	for i in range(len(vect1)):
+		toReturn.append(vect1[i] + vect2[i])
+	return toReturn
+
+
+'''
+Precision and Recall
+'''
+def precision(truePositive, falsePositive):
+	if truePositive + falsePositive == 0:
+		return 0
+	else:
+		return truePositive / (truePositive + falsePositive)
+
+def recall(truePositive, falseNegative):
+	if truePositive + falseNegative == 0:
+		return 0
+	else:
+		return truePositive / (truePositive + falseNegative)
+
+def accuracy(truePositive, trueNegative, falsePositive, falseNegative):
+	if (falsePositive + falseNegative + trueNegative + truePositive) == 0:
+		print 'accuracy 0'
+		return 0
+	else:
+		return 100*(truePositive + trueNegative) / (falsePositive + falseNegative + trueNegative + truePositive)
+'''
+Helper function for f-measure
+'''
+def fMeasure(precision, recall):
+	if precision + recall == 0:
+		return 0
+	else:
+		return 2 * precision * recall / (precision + recall)
